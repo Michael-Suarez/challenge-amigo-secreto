44791f68122088d6655d9fd79a1bb117d63e4c7e
Michael <Michael7re@gmail.com>
2025-03-12 09:45:43 -0500

    Implementar función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 let amigos = [];
 
+function añadir_amigos()
+{
+    let entrada = document.querySelector("input")
+    let nombre = entrada.value;
+    if(nombre == "")
+    {
+        alert("Por favor, inserte un nombre");
+    }
+    else
+    {
+        amigos.push(nombre);
+        entrada.value = "";
+    }
+};
