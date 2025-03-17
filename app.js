b3d8c1adbdc4d5f89b14777ef919dceaf45fd070
Michael <Michael7re@gmail.com>
2025-03-16 21:56:06 -0500

    Implementar función para sortear los amigos

--- app.js
+++ app.js
@@ -23,3 +23,19 @@ function actualizar_lista_amigos(nombre)
     li.textContent = nombre;
     lista.appendChild(li);
 };
+
+function sortear_amigos()
+{
+    let lista = document.querySelector("ul");
+    let mensaje = document.createElement("p");
+    if(amigos.length === 0)
+    {
+        alert("Por favor, inserte un nombre");
+    }
+    else
+    {
+        let elegido =Math.floor(Math.random() * amigos.length);
+        mensaje.textContent = `Tu amigo secreto es ${amigos[elegido]}`;
+        lista.replaceWith(mensaje);
+    };
+};
